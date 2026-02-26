206cd88c05224de063e38f8c249a15606fcfa6da
BrendonLee23 <brendow_prime@hotmail.com>
2026-02-26 15:17:18 -0300

    fix: Remover autenticação das rotas PUT e DELETE de mensagens

--- src/modules/messages/messages.routes.ts
+++ src/modules/messages/messages.routes.ts
@@ -19,18 +19,18 @@ router.get('/', (req, res, next) =>
   messagesController.getMessages(req, res, next)
 );
 
-// GET /messages/:id
-router.get('/:id', authMiddleware, (req, res, next) =>
+// GET /messages/:id (rota pública)
+router.get('/:id', (req, res, next) =>
   messagesController.getMessageById(req, res, next)
 );
 
-// PUT /messages/:id
-router.put('/:id', authMiddleware, (req, res, next) =>
+// PUT /messages/:id (rota pública)
+router.put('/:id', (req, res, next) =>
   messagesController.updateMessage(req, res, next)
 );
 
-// DELETE /messages/:id
-router.delete('/:id', authMiddleware, (req, res, next) =>
+// DELETE /messages/:id (rota pública)
+router.delete('/:id', (req, res, next) =>
   messagesController.deleteMessage(req, res, next)
 );
 
